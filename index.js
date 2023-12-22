41982e79ca555450634d3ec14d6f76abe618817a
Pontus <minecraftpontus@gmail.com>
2023-12-22 13:24:48 +0100

    Added a timefunction

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// Outside each elevator on each floor there is a dislay
+//On the display there should be a call button, and a log message of the status and destination floor.
+//Inside each elevator there is a display where you can call which floor you want to go to
+
 const elevator1 = {
     id: 1,
     currentFloor: 1,
-    status: ['idle', 'moving_up', 'moving_down'],
+    status: 'idle',
     destinationFloor: '',
 };
 
 const elevator2 = {
     id: 2,
     currentFloor: 1,
-    status: ['idle', 'moving_up', 'moving_down'],
+    status: 'idle',
     destinationFloor: '',
 };
 
@@ -24,7 +28,7 @@ const floors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //app.get
 
 app.get('/', (req, res) => {
-    res.send(elevators);
+    console.log(elevators);
 })
 
 app.get('/elevator/:id', (req, res) => {
@@ -34,19 +38,28 @@ app.get('/elevator/:id', (req, res) => {
     res.send(elevator);
 })
 
-app.get('/elevator/current/floor', (req, res) => {
-    res.send(elevator.currentFloor.toString());
-})
-
 //app.put
+
+//When you press a floor you want to go to(the inside display of the elevator) it displays the status and destination floor.
+
 app.put('/elevator/move/:id', (req, res) => {
+
+    const timeOutDuration = elevatorTravelTime(elevator, floor);
     const elevator = validateElevator(req, elevators)
     if (!elevator) res.status(400).send('Given elevator was not found')
 
     const floor = parseInt(req.body.floor);
     elevator.destinationFloor = floor;
-    res.send(`Going to floor ${elevator.destinationFloor}...`);
+    
+    console.log(`Going to floor ${elevator.destinationFloor}!`);
+
+    setTimeout(() => {
+        elevator.currentFloor = floor;
+        res.send(`You have arrived at floor ${elevator.currentFloor}!`);
+    }, timeOutDuration);
 })
+
+
 
 //functions
 
@@ -55,6 +68,16 @@ function validateElevator(req, elevators) {
     return elevator;
 }
 
+function elevatorTravelTime(elevator, floor) {
+
+    const floorsToTravel = Math.abs(elevator.currentFloor - floor);
+    const timeOutDuration = floorsToTravel * 1000;
+    return timeOutDuration;
+}
+
+function validateStatus() {
+
+}
 // function getElevatorStatus(elevator) {
 //     //Retrieve the current location and status of all elevators.
 //     return new Promise((resolve, reject) => {
